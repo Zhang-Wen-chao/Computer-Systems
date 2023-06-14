98087dfe7025e7a399b5ecd39cdea3f0e62e8fa4
Zhang-Wen-chao <zhangwch2022@shanghaitech.edu.cn>
2023-06-14 19:17:13 +0800

    add files

---
+++ CPP/week15/examples/const_cast.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int value1 = 100;
+    const int value2 = 200;
+    cout << "value1 = " << value1 << endl;
+    cout << "value2 = " << value2 << endl;
+
+    // int * pv1 = &value1;
+    int * pv1 = const_cast<int *>(&value1);
+    // int * pv2 = &value2; // error
+    int * pv2 = const_cast<int *>(&value2);
+
+    (*pv1)++;
+    (*pv2)++;
+
+    cout << "value1 = " << (*pv1) << endl;
+    cout << "value2 = " << (value2) << endl;
+    
+    // // int & v2 = value2; //error
+    // int& v2 = const_cast<int&>(value2);
+    // v2++;
+    // cout << "value2 = " << value2 << endl;
+
+    // cout << "*pv2 = " << (*pv2) << endl;
+    // cout << "v2 = " << v2 << endl;
+
+   return 0;
+}
+
+
