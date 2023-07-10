c84abe8c8205d90eab8e424ec9d6022e0e4eca84
Zhang-Wen-chao <zhangwch2022@shanghaitech.edu.cn>
2023-07-10 10:32:48 +0000

    update

--- myLeetCode/Binary_Tree/Modification and construction of Binary tree/0106. Construct Binary Tree from Inorder and Postorder Traversal.cpp
+++ myLeetCode/Binary_Tree/Modification and construction of Binary tree/0106. Construct Binary Tree from Inorder and Postorder Traversal.cpp
@@ -4,13 +4,6 @@
 #include <unordered_map>
 #include <iostream>
 
-struct TreeNode {
-    int val;
-    TreeNode *left;
-    TreeNode *right;
-    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-};
-
 class Solution {
 public:
     TreeNode* buildTree(std::vector<int>& inorder, std::vector<int>& postorder) {
