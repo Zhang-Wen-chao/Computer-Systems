8e05d4fe6225b9e8584f1a8042a8efd4dd866a25
Zhang-Wen-chao <zhangwch2022@shanghaitech.edu.cn>
2023-07-19 01:08:02 +0800

    update

--- myLeetCode/Greedy/455. Assign Cookies.cpp
+++ myLeetCode/Greedy/455. Assign Cookies.cpp
@@ -28,6 +28,6 @@ int main() {
     std::vector<int> greed = {1,2}; // greed factors of children
     std::vector<int> cookies = {1,2,3}; // sizes of cookies
     std::cout << findContentChildren(greed, cookies) << std::endl;
-    
+
     return 0;
 }
