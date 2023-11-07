e94ca5f14deb9707dbfe46e7eb0ce1973a083309
Zhang-Wen-chao <zhangwch2022@shanghaitech.edu.cn>
2023-11-07 20:45:50 +0800

    update

--- C++/代码随想录/Array/977. Squares of a Sorted Array.cpp
+++ C++/代码随想录/Array/977. Squares of a Sorted Array.cpp
@@ -5,26 +5,27 @@
 #include <iostream>
 #include <vector>
 
-std::vector<int> sortedSquares_Double_pointer(std::vector<int> &nums) {
+std::vector<int> sortedSquares(std::vector<int>& nums) {
     int n = nums.size();
     std::vector<int> ans(n);
-    for (int left = 0, right = n - 1, pos = n - 1; left <= right;) {
-        if (nums[left] * nums[left] > nums[right] * nums[right]) {
+    for (int left = 0, right = n-1, pos = n-1; left <= right; ){
+        if (nums[left] * nums[left] >= nums[right] * nums[right]){
             ans[pos] = nums[left] * nums[left];
-            ++left;
-        } else {
-            ans[pos] = nums[right] * nums[right];
-            --right;
+            pos --;
+            left ++;
         }
-        --pos;
+        else {
+            ans[pos] = nums[right] * nums[right];
+            pos --;
+            right --;
+        }
     }
     return ans;
 }
-问一下chatgpt，用left++行不行？
 
 int main() {
     std::vector<int> nums = {-4, -1, 0, 3, 10};
-    std::vector<int> ans = sortedSquares_Double_pointer(nums);
+    std::vector<int> ans = sortedSquares(nums);
     for (int num : ans) {
         std::cout << num << " ";
     }
