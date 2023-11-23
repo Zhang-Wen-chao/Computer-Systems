cefd3cb6a224ed44665dd432ecf92ebd92adc3c4
Zhang-Wen-chao <zhangwch2022@shanghaitech.edu.cn>
2023-11-23 21:34:03 +0800

    update

--- C++/代码随想录/Hashtable/349. Intersection of Two Arrays.py
+++ C++/代码随想录/Hashtable/349. Intersection of Two Arrays.py
@@ -2,8 +2,7 @@ from typing import List
 
 class Solution:
     def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        set1 = set(nums1)
-        set2 = set(nums2)
+        set1, set2 = set(nums1), set(nums2)
         return list(set1 & set2)
 
 sol = Solution()
