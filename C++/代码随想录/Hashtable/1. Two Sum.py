2984277c16b981d2db886a48261faee038a5e297
Solomon Sharp <zhangwch2022@shanghaitech.edu.cn>
2024-07-05 14:40:55 +0800

    modify variable name

--- C++/代码随想录/Hashtable/1. Two Sum.py
+++ C++/代码随想录/Hashtable/1. Two Sum.py
@@ -2,12 +2,12 @@ from typing import List
 
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        num_map = {}
+        num_dict = {}
         for i, num in enumerate(nums):
             complement = target - num
-            if complement in num_map:
-                return [num_map[complement], i]
-            num_map[num] = i
+            if complement in num_dict:
+                return [num_dict[complement], i]
+            num_dict[num] = i
         return []
 
 sol = Solution()
